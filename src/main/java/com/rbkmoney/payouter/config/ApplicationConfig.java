338682b81fd74335b21743d09cbb759376b2c47f
Pavel Popov <popoffpavel@yandex.ru>
2017-12-14 16:54:32 +0300

    PAYOUT-3: Merge DaoConf with ApplicationConf

--- src/main/java/com/rbkmoney/payouter/config/ApplicationConfig.java
+++ src/main/java/com/rbkmoney/payouter/config/ApplicationConfig.java
@@ -3,7 +3,9 @@ package com.rbkmoney.payouter.config;
 import com.rbkmoney.damsel.accounter.AccounterSrv;
 import com.rbkmoney.damsel.domain_config.RepositoryClientSrv;
 import com.rbkmoney.damsel.payment_processing.PartyManagementSrv;
+import com.rbkmoney.payouter.domain.Sht;
 import com.rbkmoney.woody.thrift.impl.http.THSpawnClientBuilder;
+import org.jooq.Schema;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,6 +32,11 @@ public class ApplicationConfig {
     public AccounterSrv.Iface shumwayClient(@Value("${service.shumway.url}") Resource resource) throws IOException {
         return new THSpawnClientBuilder()
                 .withAddress(resource.getURI()).build(AccounterSrv.Iface.class);
+    }
+
+    @Bean
+    public Schema schema() {
+        return Sht.SHT;
     }
 
 }
