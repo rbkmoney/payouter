3ab762042d6af96cf1d8ff1bbfe150f7494eb445
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2018-01-11 18:17:57 +0300

    PAYOUT-42: Remove cash description

--- src/main/java/com/rbkmoney/payouter/handler/EventSinkHandler.java
+++ src/main/java/com/rbkmoney/payouter/handler/EventSinkHandler.java
@@ -2,9 +2,7 @@ package com.rbkmoney.payouter.handler;
 
 import com.rbkmoney.damsel.base.InvalidRequest;
 import com.rbkmoney.damsel.payout_processing.*;
-import com.rbkmoney.payouter.domain.tables.pojos.CashFlowDescription;
 import com.rbkmoney.payouter.domain.tables.pojos.PayoutEvent;
-import com.rbkmoney.payouter.service.CashFlowDescriptionService;
 import com.rbkmoney.payouter.service.EventSinkService;
 import com.rbkmoney.payouter.util.DamselUtil;
 import org.apache.thrift.TException;
@@ -20,12 +18,10 @@ import java.util.stream.Collectors;
 public class EventSinkHandler implements EventSinkSrv.Iface {
 
     private final EventSinkService eventSinkService;
-    private final CashFlowDescriptionService cashFlowDescriptionService;
 
     @Autowired
-    public EventSinkHandler(EventSinkService eventSinkService, CashFlowDescriptionService cashFlowDescriptionService) {
+    public EventSinkHandler(EventSinkService eventSinkService) {
         this.eventSinkService = eventSinkService;
-        this.cashFlowDescriptionService = cashFlowDescriptionService;
     }
 
     @Override
@@ -46,16 +42,6 @@ public class EventSinkHandler implements EventSinkSrv.Iface {
         List<Event> eventList = eventSinkService.getEvents(after, eventRange.getLimit()).stream()
                 .map(DamselUtil::toDamselEvent)
                 .collect(Collectors.toList());
-
-        for (Event event : eventList) {
-            for (PayoutChange pc : event.getPayload().getPayoutChanges()) {
-                if (pc.isSetPayoutCreated()) {
-                    Payout payout = pc.getPayoutCreated().getPayout();
-                    List<CashFlowDescription> cashFlowDescriptions = cashFlowDescriptionService.get(Long.parseLong(payout.getId()));
-                    payout.setCashFlowDescriptions(DamselUtil.toDamselCashFlowDescription(cashFlowDescriptions));
-                }
-            }
-        }
 
         return eventList;
     }
