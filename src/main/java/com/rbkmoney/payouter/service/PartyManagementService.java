e56acf3a547421dad0b69c5a838c020276248d7c
Pavel Popov <popoffpavel@yandex.ru>
2018-01-17 09:10:51 +0300

    Party blocking

--- src/main/java/com/rbkmoney/payouter/service/PartyManagementService.java
+++ src/main/java/com/rbkmoney/payouter/service/PartyManagementService.java
@@ -1,22 +1,22 @@
 package com.rbkmoney.payouter.service;
 
 import com.rbkmoney.damsel.domain.CategoryType;
-import com.rbkmoney.damsel.domain.Contract;
 import com.rbkmoney.damsel.domain.Party;
 import com.rbkmoney.damsel.domain.Shop;
-import com.rbkmoney.damsel.domain_config.Reference;
+import com.rbkmoney.damsel.msgpack.Value;
 import com.rbkmoney.payouter.exception.InvalidStateException;
 import com.rbkmoney.payouter.exception.NotFoundException;
 import com.rbkmoney.payouter.model.PayoutToolData;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 
 public interface PartyManagementService {
 
     Party getParty(String partyId, Instant timestamp) throws NotFoundException;
 
     Shop getShop(String partyId, String shopId, Instant timestamp) throws NotFoundException;
+
+    Value getMetaData(String partyId, String namespace) throws NotFoundException;
 
     PayoutToolData getPayoutToolData(String partyId, String shopId) throws InvalidStateException, NotFoundException;
 
