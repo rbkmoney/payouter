661c634499eb6d4ffd3123ed465d8672aa8f6dad
Pavel Popov <popoffpavel@yandex.ru>
2018-02-22 11:50:07 +0300

    Fix migrations script

--- src/main/resources/db/migration/V17__1.0.5_report_improvements.sql
+++ src/main/resources/db/migration/V17__1.0.5_report_improvements.sql
@@ -1,3 +1,9 @@
 ALTER TABLE sht.report RENAME payoutIds TO payout_ids;
-ALTER TABLE sht.report ADD COLUMN encoding CHARACTER VARYING NOT NULL;
-ALTER TABLE sht.report ADD COLUMN subject CHARACTER VARYING NOT NULL;
+
+ALTER TABLE sht.report ADD COLUMN subject CHARACTER VARYING;
+UPDATE sht.report SET subject = 'Выплаты для резидентов, сгенерированные ' || to_char(created_at, 'dd.MM.yyyy');
+ALTER TABLE sht.report ALTER COLUMN subject SET NOT NULL;
+
+ALTER TABLE sht.report ADD COLUMN encoding CHARACTER VARYING;
+UPDATE sht.report SET encoding = 'Windows-1251';
+ALTER TABLE sht.report ALTER COLUMN encoding SET NOT NULL;
