21a1222916677cb57ca71cb21114ea5dd7fc66c6
Inal Arsanukaev <inalarsanukaev@MacBook-Pro-Inal.local>
2018-03-02 16:26:31 +0300

    Replace method call for variable

--- src/main/java/com/rbkmoney/payouter/service/impl/ResidentsMailContentServiceImpl.java
+++ src/main/java/com/rbkmoney/payouter/service/impl/ResidentsMailContentServiceImpl.java
@@ -31,8 +31,9 @@ public class ResidentsMailContentServiceImpl extends MailContentServiceImpl{
         reportDescription.put("from_date", payout.getFromTime().format(dateTimeFormatter));
         reportDescription.put("to_date", payout.getToTime().format(dateTimeFormatter));
         List<CashFlowDescription> cashFlowDescriptions = cashFlowDescriptionDao.get(String.valueOf(payout.getId()));
-        reportDescription.put("payment_sum", getFormattedAmount(cashFlowDescriptions.stream().filter(cfd -> cfd.getCashFlowType() == CashFlowType.payment).findFirst().get().getAmount()));
-        reportDescription.put("rbk_fee_sum", getFormattedAmount(cashFlowDescriptions.stream().filter(cfd -> cfd.getCashFlowType() == CashFlowType.payment).findFirst().get().getFee()));
+        CashFlowDescription paymentCashFlowDescription = cashFlowDescriptions.stream().filter(cfd -> cfd.getCashFlowType() == CashFlowType.payment).findFirst().get();
+        reportDescription.put("payment_sum", getFormattedAmount(paymentCashFlowDescription.getAmount()));
+        reportDescription.put("rbk_fee_sum", getFormattedAmount(paymentCashFlowDescription.getFee()));
         cashFlowDescriptions.stream().filter(cfd -> cfd.getCashFlowType() == CashFlowType.refund).findFirst().ifPresent(x -> reportDescription.put("refund_sum", getFormattedAmount(x.getAmount())));
         reportDescription.put("fee_sum", getFormattedAmount(payout.getFee()));
         return reportDescription;
