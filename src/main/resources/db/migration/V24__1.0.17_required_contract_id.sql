ae0e126f1209ffbec4b72aa90b1e930bdd2a4f1f
Pavel Popov <popoffpavel@yandex.ru>
2018-06-19 14:09:10 +0300

    Add contract_id in payout_event table

--- src/main/resources/db/migration/V24__1.0.17_required_contract_id.sql
+++ src/main/resources/db/migration/V24__1.0.17_required_contract_id.sql
@@ -13,3 +13,7 @@ UPDATE sht.payout SET contract_id = (SELECT contract_id FROM sht.payment WHERE p
 
 ALTER TABLE sht.payout
   ALTER COLUMN contract_id SET NOT NULL;
+
+ALTER TABLE sht.payout_event ADD COLUMN contract_id CHARACTER VARYING;
+
+UPDATE sht.payout_event SET contract_id = (SELECT contract_id FROM sht.payout WHERE cast(id AS CHARACTER VARYING) = payout_event.payout_id);
