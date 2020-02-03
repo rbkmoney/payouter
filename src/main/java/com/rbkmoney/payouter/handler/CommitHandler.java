0d817e79527a9bb64aa2f2f8f273e46411694feb
Pavel Popov <popoffpavel@yandex.ru>
2020-02-03 21:54:46 +0300

    BJ-782: Fixes in claim-commiter validation

--- src/main/java/com/rbkmoney/payouter/handler/CommitHandler.java
+++ src/main/java/com/rbkmoney/payouter/handler/CommitHandler.java
@@ -1,13 +1,9 @@
 package com.rbkmoney.payouter.handler;
 
-import com.rbkmoney.damsel.claim_management.InvalidChangeset;
-import com.rbkmoney.damsel.claim_management.PartyNotFound;
-import org.apache.thrift.TException;
-
 public interface CommitHandler<T> {
 
-    void accept(String partyId, T claim) throws PartyNotFound, InvalidChangeset, TException;
+    void accept(String partyId, String shopId, T claim);
 
-    void commit(String partyId, T claim) throws TException;
+    void commit(String partyId, String shopId, T claim);
 
 }
